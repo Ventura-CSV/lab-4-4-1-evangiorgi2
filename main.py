e249af8b3db9d23ebb3654e3a8ddec0494fa91de
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 15:51:12 -0700

    Correct number complete

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 def main():
 
-    number = int(input('Enter a number: '))
-    while True (number > 0 or number < 100):
-        print('You printed the correct number')
-    else:
-        number = int(input('Input a number between 0 and 100'))
-   
-    print(number)
+        number = int(input('Enter a number: '))
+        
+        while (number < 0 or number > 100):
+            number = int(input('The number should be between 0 and 100: '))
+        print(number)
 
-
-    return number
+        return number
 
 
 if __name__ == '__main__':
