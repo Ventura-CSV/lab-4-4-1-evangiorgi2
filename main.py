cc113f7af6e598b903017ba0f653e70bf5284368
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 15:35:59 -0700

    Error With Closing While loop

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
 
+    number = int(input('Enter a number: '))
+    while True (number > 0 or number < 100):
+        print('You printed the correct number')
+    else:
+        number = int(input('Input a number between 0 and 100'))
+   
     print(number)
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
+
     return number
 
 
